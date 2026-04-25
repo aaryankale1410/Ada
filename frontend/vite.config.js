6b00213519c297ba18dcbdcde886cddfea7f42c5
aaryankale1410 <aaryankale1410@gmail.com>
2026-04-26 01:23:02 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,37 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['icon-192.png', 'icon-512.png'],
-      manifest: {
-        name: 'Aaryan & Akshada',
-        short_name: 'Us',
-        description: 'Our special place',
-        start_url: '/',
-        scope: '/',
-        theme_color: '#07060e',
-        background_color: '#07060e',
-        display: 'standalone',
-        icons: [
-          {
-            src: '/icon-192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: '/icon-512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
-  ],
+  plugins: [react()],
   server: {
     port: 5173,
     proxy: {
